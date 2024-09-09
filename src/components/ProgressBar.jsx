10dd982c0beb8ac6cbe92cb0f627299a249a5ac9
AdamNgazzou <117188411+AdamNgazzou@users.noreply.github.com>
2024-09-09 20:22:30 +0100

    Add files via upload

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,16 @@
+// ProgressBar.js
+import React from 'react';
+import './ProgressBar.css';
+
+const ProgressBar = ({ progress }) => {
+    return (
+        <div className='progress-bar'>
+            <div
+                className='progress-bar-fill'
+                style={{ width: `${progress}%` }}
+            ></div>
+        </div>
+    );
+};
+
+export default ProgressBar;
